918ff5d28fedb5875c3a0952fd1577104d3a3e61
agustinmendizabal <166071177+agustinmendizabal@users.noreply.github.com>
2024-06-25 00:14:26 -0300

    Update carrito.js

--- js/carrito.js
+++ js/carrito.js
@@ -1,14 +1,3 @@
-let recuperoStorage = localStorage.getItem("miCarrito");
-let storageToParse = JSON.parse(recuperoStorage);
-let contenedor = document.querySelector('main')
-let contenedor_cart = document.querySelector('.container_producto')
-let finalizar = document.querySelector('.finalizar')
-
-  .then(function(data){
-    console.log(data);
-    let informacioncategoria = data;
-    let navegador = document.querySelector('.navegador')
-    
 navegador.innerHTML =  ` 
   <li><a href="./index.html">HOME</a></li>
   <li><a href="./category.html?categories=${informacioncategoria.category="electronics"}">ELECTRONICOS</a></li>
